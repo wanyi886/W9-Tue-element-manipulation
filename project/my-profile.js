ef6e2fad942780c6798f1501f7ac94ff35b880a8
Wanyi <wanyilee0805@gmail.com>
2021-10-19 14:56:17 -0700

    countdown

--- project/my-profile.js
+++ project/my-profile.js
@@ -10,3 +10,23 @@ textArr.forEach((ele) => {
   detail.innerText = ele;
   aboutDiv.appendChild(detail);
 })
+
+let now = new Date();
+now = now.getTime()
+let christmas = new Date("December 25, 2021")
+christmas = christmas.getTime();
+let result = christmas - now
+let daysleft = result/1000/86400
+console.log(daysleft)
+
+let days = document.querySelector("#Days")
+days.innerHTML = `Days: ${Math.floor(daysleft)}`
+
+let hoursleft = result/1000/3600;
+let minutesleft = result/1000/60;
+
+let hours = document.querySelector("#Hours")
+hours.innerHTML = `Hours: ${Math.floor(hoursleft)}`
+
+let mins = document.querySelector("#Minutes")
+mins.innerHTML = `Minutes: ${Math.floor(minutesleft)}`
